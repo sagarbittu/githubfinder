a9006170c1f167424d7450ef043c277ae7d3048e
Sagar Singh <sagar.bittu12@gmail.com>
2020-07-12 21:34:01 +0530

    class component, component state

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,29 +1,28 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-  constructor () {
-    super ()
-    this.state = {
-      id: 'id',
-      login: 'sagarbittu',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/17125746?v=4',
-      html_url: 'https://github.com/sagarbittu'
-    }
+  state = {
+    id: 'id',
+    login: 'sagarbittu',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/17125746?v=4',
+    html_url: 'https://github.com/sagarbittu'
   }
   
   render () {
+    const { avatar_url, html_url, login } = this.state
+    
     return (
       <div className='card text-center'>
         <img
-          src={this.state.avatar_url} 
+          src={avatar_url} 
           alt=''
           className='round-img'
           style={{ width: '60px' }}
         />
-        <h3>{this.state.login}</h3>
+        <h3>{login}</h3>
         
         <div>
-          <a href={this.state.html_url} className='btn btn-dark btn-sm my-1'>
+          <a href={html_url} className='btn btn-dark btn-sm my-1'>
             More
           </a>
         </div>
