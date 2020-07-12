80685e0b7f22c5f7cbd4286f7d985234f5cfbe2d
Sagar Singh <sagar.bittu12@gmail.com>
2020-07-12 22:56:06 +0530

    Destructuring props in functional components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const UserItem = (props) => {
-  const { avatar_url, html_url, login } = props.user
-  
+const UserItem = ({ user: { avatar_url, html_url, login } }) => {
   return (
     <div className='card text-center'>
       <img
