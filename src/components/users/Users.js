232c8a0d67a5a37fe53e84da0efd33be37876ded
Sagar Singh <sagar.bittu12@gmail.com>
2020-07-12 23:47:48 +0530

    Http requests and updating state

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,31 +2,10 @@ import React, { Component } from 'react'
 import UserItem from './UserItem'  
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '17125746',
-        login: 'sagarbittu',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/17125746?v=4',
-        html_url: 'https://github.com/sagarbittu'
-      }, {
-        id: '1',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-      }, {
-        id: '2',
-        login: 'defunkt',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt'
-      } 
-    ]
-  }
-  
   render () {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
